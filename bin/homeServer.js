ed2a4cc2b6bcf7e9af85f233989f8a36c0b01607
hantianxiang <tianxiang.htx@alibaba-inc.com>
2017-07-29 18:10:18 +0800

    homeServer 1.0

---
+++ bin/homeServer.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+(function () {
+  "use strict";
+
+  var homeServer = require('../lib')
+    , port = process.argv[2] || 3000
+    , server
+    ;
+
+  server = homeServer.listen(port, function () {
+    console.log(
+        'Serving '
+      + homeServer.path
+      + ' via HTTP on '
+      + server.address().address
+      + ':'
+      + server.address().port
+    );
+  });
+}());
