96ec13a194dc96b53068ea1783634c7cc9b6266a
losthanKKK <18610611028@163.com>
2017-11-08 14:39:38 +0800

    beautify

--- bin/homeServer.js
+++ bin/homeServer.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
-(function () {
-  "use strict";
 
-  var homeServer = require('../lib')
-    , port = process.argv[2] || 3000
-    , server
-    ;
+(function() {
+    "use strict";
 
-  server = homeServer.listen(port, function () {
-    console.log(
-        'Serving '
-      + homeServer.path
-      + ' via HTTP on '
-      + server.address().address
-      + ':'
-      + server.address().port
-    );
-  });
+    var homeServer = require('../lib'),
+        port = process.argv[2] || 3000,
+        server;
+
+    server = homeServer.listen(port, function() {
+        console.log('Serving ' + homeServer.path + ' via HTTP on ' + server.address().address + ':' + server.address().port);
+    });
 }());
